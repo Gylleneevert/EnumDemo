181245c310facd0c1d1185a0f5852331642699b6
GylleneEvert <robert.jakobsen84@gmail.com>
2023-09-11 10:27:50 +0200

    Lagt till 2 st Console.WriteLine

--- EnumDemo/Program.cs
+++ EnumDemo/Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello version.2");
+            Console.WriteLine("Rad 2");
+            Console.WriteLine("Rad 4");
         }
     }
 }
